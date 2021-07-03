b37d70ce81925b0c9e259d55283d512fc756c94e
Iftekhar Hasan <iftekharhasanrifat@gmail.com>
2021-07-03 20:49:04 +0600

    dropdown addded

--- src/components/AddPrograms/AddPrograms.js
+++ src/components/AddPrograms/AddPrograms.js
@@ -1,17 +1,24 @@
 import React from 'react';
-
+import './AddPrograms.css'
 const AddPrograms = () => {
     return (
         <div className='write'>
             <h2 className='text-center mb-5'>Publish Programs</h2>
             <form className="writeForm">
                 <div className="writeFormGroup">
-                    <input type="text" placeholder="Category" className="writeInput" autoFocus={true} />
+                    <select className="categoryDropdown" name="category" id="categories">
+                        <option value="Know Your Country">Know Your Country</option>
+                        <option value="Cartoon Songs">Cartoon Songs</option>
+                        <option value="Poems">Poems</option>
+                        <option value="Little Genious">Little Genious</option>
+                        <option value="Country Songs">Country Songs</option>
+                    </select>
+                    {/* <input type="text" placeholder="Category" className="writeInput" autoFocus={true} /> */}
                 </div>
                 <div className="writeFormGroup">
                     <textarea placeholder="Link" type="text" className="writeInput writeText"></textarea>
                 </div>
-                <button className='writeSubmit'>Publish</button>
+                <button className='programSubmit'>Publish</button>
             </form>
         </div>
     );
--- src/components/UpciomingPrograms/UpciomingPrograms.js
+++ src/components/UpciomingPrograms/UpciomingPrograms.js
@@ -4,7 +4,12 @@ const UpciomingPrograms = () => {
     return (
         <div className="write">
             <h2 className='text-center mb-5'>Publish Upcoming Program Banner</h2>
-             <form className="writeForm">
+            <div className='imgContainer'>
+                <img className="writeImg"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Shaqi_jrvej.jpg/1200px-Shaqi_jrvej.jpg"
+                    alt="" />
+            </div>
+            <form className="writeForm">
                 <div className="writeFormGroup">
                     <label htmlFor="upcomingFileInput">
                         <i className="writeIcon fas fa-plus"></i>
